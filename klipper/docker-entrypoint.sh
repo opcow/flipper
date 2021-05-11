58ba9db17f4f328dfd4725bb591244d6ba8e7d41
Mitch Crane <mitch.crane@gmail.com>
2021-05-11 15:52:50 -0400

    use exec

--- klipper/docker-entrypoint.sh
+++ klipper/docker-entrypoint.sh
@@ -2,8 +2,8 @@
 # [ -f "/config/printer.cfg" ] || cp /opt/klipper/printer.cfg /config/
 # [ -f "/config/macros.cfg" ] || cp /opt/klipper/macros.cfg /config/
 
-${KLIPPY_ENV}/bin/python ${KLIPPER_HOME}/klipper/klippy/klippy.py ${CONFIG_PATH}/printer.cfg -l /tmp/klippy.log -a /tmp/klippy_uds &
-${MOONRAKER_ENV}/bin/python ${KLIPPER_HOME}/moonraker/moonraker/moonraker.py -c ${CONFIG_PATH}/moonraker.conf
+exec ${KLIPPY_ENV}/bin/python ${KLIPPER_HOME}/klipper/klippy/klippy.py ${CONFIG_PATH}/printer.cfg -l /tmp/klippy.log -a /tmp/klippy_uds &
+exec ${MOONRAKER_ENV}/bin/python ${KLIPPER_HOME}/moonraker/moonraker/moonraker.py -c ${CONFIG_PATH}/moonraker.conf
 
 # exec "$@"
 ########
