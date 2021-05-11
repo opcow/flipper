bd14f881dd58da427b5db1c84bafd45ea3c07141
Mitch Crane <mitch.crane@gmail.com>
2021-05-11 11:02:28 -0400

    merged klipper and moonraker containers mooraker works best this way there are workarounds, but it's just not worth the trouble

--- klipper/docker-entrypoint.sh
+++ klipper/docker-entrypoint.sh
@@ -2,5 +2,8 @@
 # [ -f "/config/printer.cfg" ] || cp /opt/klipper/printer.cfg /config/
 # [ -f "/config/macros.cfg" ] || cp /opt/klipper/macros.cfg /config/
 
-exec "$@"
+${KLIPPY_ENV}/bin/python ${KLIPPER_HOME}/klipper/klippy/klippy.py ${CONFIG_PATH}/printer.cfg -l /tmp/klippy.log -a /tmp/klippy_uds &
+${MOONRAKER_ENV}/bin/python ${KLIPPER_HOME}/moonraker/moonraker/moonraker.py -c ${CONFIG_PATH}/moonraker.conf
+
+# exec "$@"
 ########
