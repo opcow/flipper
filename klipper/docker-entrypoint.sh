550bd7410c861db678b572aba4dcf02c06232980
Mitch Crane <mitch.crane@gmail.com>
2021-05-12 15:50:05 -0400

    allow for running a different command

--- klipper/docker-entrypoint.sh
+++ klipper/docker-entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # [ -f "/config/printer.cfg" ] || cp /opt/klipper/printer.cfg /config/
 # [ -f "/config/macros.cfg" ] || cp /opt/klipper/macros.cfg /config/
-
-exec ${KLIPPY_ENV}/bin/python ${KLIPPER_HOME}/klipper/klippy/klippy.py ${CONFIG_PATH}/printer.cfg -l /tmp/klippy.log -a /tmp/klippy_uds &
-exec ${MOONRAKER_ENV}/bin/python ${KLIPPER_HOME}/moonraker/moonraker/moonraker.py -c ${CONFIG_PATH}/moonraker.conf
-
-# exec "$@"
-########
+if [ $# -eq 0 ]; then
+  exec ${KLIPPY_ENV}/bin/python ${KLIPPER_HOME}/klipper/klippy/klippy.py ${CONFIG_PATH}/printer.cfg -l /tmp/klippy.log -a /tmp/klippy_uds &
+  exec ${MOONRAKER_ENV}/bin/python ${KLIPPER_HOME}/moonraker/moonraker/moonraker.py -c ${CONFIG_PATH}/moonraker.conf
+else
+  exec "$@"
+fi
