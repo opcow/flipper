2453c5e5071832044cfa0db041c7ffb86293272d
Mitch Crane <mitch.crane@gmail.com>
2021-05-13 12:02:51 -0400

    rm commented out old code

--- klipper/docker-entrypoint.sh
+++ klipper/docker-entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-# [ -f "/config/printer.cfg" ] || cp /opt/klipper/printer.cfg /config/
-# [ -f "/config/macros.cfg" ] || cp /opt/klipper/macros.cfg /config/
 if [ $# -eq 0 ]; then
   sudo chown ${KLIPPER_USER}:${KLIPPER_USER} /config/*
   exec ${KLIPPY_ENV}/bin/python ${KLIPPER_HOME}/klipper/klippy/klippy.py ${CONFIG_PATH}/printer.cfg -l /tmp/klippy.log -a /tmp/klippy_uds &
